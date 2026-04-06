abd49b91a38e26862d88a08f2637c16e1e23610a
juliawalter-coder <julia.walter@globant.com>
2026-04-06 08:55:25 +0200

    Add Page Object Model - activity 1

--- src/pages/web/herokuapp/AddRemoveElementsPage.ts
+++ src/pages/web/herokuapp/AddRemoveElementsPage.ts
@@ -5,6 +5,19 @@ class AddRemoveElementsPage extends WebBasePage {
   public async navigate(): Promise<void> {
     await super.navigate('add_remove_elements/');
   }
+
+  get addElementButton(){
+    return $('#content > div > button');
+  }
+
+  get deleteElement() {
+    return $('button[class="added-manually"]');
+  }
+
+  async ClickAddElementButton(){
+    await this.addElementButton.click();
+  }
+
 }
 
 export default new AddRemoveElementsPage();
--- src/tests/web/herokuapp/level1AddRemoveElements.spec.ts
+++ src/tests/web/herokuapp/level1AddRemoveElements.spec.ts
@@ -9,22 +9,15 @@ describe('Level 1 - Button click scenario', () => {
 
   it('ACT001 - Clicking add element button should add another delete button', async () => {;
 
-    //await browser.url('add_remove_elements/');
-    const addElement = await $('#content > div > button');
-    const deleteElement = await $('button[class="added-manually"]');
-    await expect(deleteElement).not.toBeDisplayed();
-    //let isDisplayedDeleteElement = await deleteElement.isDisplayed();
-    //expect(isDisplayedDeleteElement).toEqual(false);
-    await addElement.click();
+    await expect(AddRemoveElementsPage.deleteElement).not.toBeDisplayed();
+    await AddRemoveElementsPage.ClickAddElementButton();
+    await expect(AddRemoveElementsPage.deleteElement).toHaveText('Delete');
+    await expect(AddRemoveElementsPage.deleteElement).toBeDisplayed();
 
-    // expect(isDisplayedDeleteElement).to.equal(true);
-    await expect(deleteElement).toHaveText('Delete');
-    await expect(deleteElement).toBeDisplayed();
     //Access page
     //Identify the button - click on button
     //Identify the delete button on the screen
-
     //await fistnama.setValue();
-    //await browser.pause(500); Ponemos solo para
+    //await browser.pause(500);
   })
 })
